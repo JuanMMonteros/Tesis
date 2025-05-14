39699b5b11143df204dab153334e744a6c281155
Lbarberon <113266217+Lbarberon@users.noreply.github.com>
2025-05-14 18:59:58 -0300

    Add files via upload

--- Messuerment_Setup/PyVisa/instrument_config/config_functions.py
+++ Messuerment_Setup/PyVisa/instrument_config/config_functions.py
@@ -194,7 +194,7 @@ def PVT(instrument, directorio, plot):
                 phase_data = np.array(phase_data)  # Convierte a arreglo NumPy
 
                 # Genera un arreglo de time# Genera un arreglo de puntos temporales (de 0 a 10 ms)
-                time_points = np.linspace(0, 10e-3, len(phase_data)) * 1e3  # Convierte a milisegundos
+                time = np.linspace(0, 10e-3, len(phase_data)) * 1e3  # Convierte a milisegundos
 
                 # Busca el próximo número disponible para el archivo
                 i = 1
@@ -205,7 +205,7 @@ def PVT(instrument, directorio, plot):
                 output_path = os.path.join(directorio, filename)
 
                 # Guarda los datos en un archivo CSV
-                pd.DataFrame({'Time (ms)': time_points, 'Phase (º)': phase_data}).to_csv(output_path, index=False)
+                pd.DataFrame({'Time (ms)': time, 'Phase (º)': phase_data}).to_csv(output_path, index=False)
                 print(f"Datos guardados en '{output_path}'.")
 
                 if plot:
@@ -239,11 +239,11 @@ def TimeOverview(instrument, directorio, plot):
             num_bytes = int(raw_response[2:2 + num_digits].decode())  # Longitud de los datos en bytes
             header_length = 2 + num_digits  # Longitud del encabezado
             data_bytes = raw_response[header_length:header_length + num_bytes]  # Extrae los datos
-            phase_data = struct.unpack(f'<{num_bytes // 4}f', data_bytes)  # Convierte bytes a flotantes
-            phase_data = np.array(phase_data)  # Convierte a arreglo NumPy
+            time_overview_data = struct.unpack(f'<{num_bytes // 4}f', data_bytes)  # Convierte bytes a flotantes
+            time_overview_data = np.array(time_overview_data)  # Convierte a arreglo NumPy
 
             # Genera un arreglo de time# Genera un arreglo de puntos temporales (de 0 a 10 ms)
-            time_points = np.linspace(0, 10e-3, len(phase_data)) * 1e3  # Convierte a milisegundos
+            time = np.linspace(0, 10e-3, len(time_overview_data)) * 1e3  # Convierte a milisegundos
 
             # Busca el próximo número disponible para el archivo
             i = 1
@@ -254,7 +254,7 @@ def TimeOverview(instrument, directorio, plot):
             output_path = os.path.join(directorio, filename)
 
             # Guarda los datos en un archivo CSV
-            pd.DataFrame({'Time (ms)': time_points, 'Amplitud (dBm)': time_overview_data}).to_csv(output_path, index=False)
+            pd.DataFrame({'Time (ms)': time, 'Amplitud (dBm)': time_overview_data}).to_csv(output_path, index=False)
             print(f"Datos guardados en '{output_path}'.")
 
             if plot:
@@ -264,5 +264,71 @@ def TimeOverview(instrument, directorio, plot):
         return f"Medicion Exitosa"
 
     except Exception as e:
-        return f"Error en Phase vs Time: {e}"  # Solicitar datos
+        return f"Error en TimeOverview: {e}"  # Solicitar datos
+
+
+def Pulse_Trace(instrument, directorio, plot):
+    # --- Inicialización de variables para almacenar datos ---
+    pulse_data = np.array([])  # Datos de fase
+    time = np.array([])  # vector de tiempo
+    try:
+        print("\n--- Capturando Pulse Trace ---")
+        print("Configurando Pulse Trace: Umbral -4 dBm, Rango 15 μs, Reference Level 0 dBm.")
+        send_command(instrument, ':SENSe:PULSe:THReshold -4')  # Establece el umbral para detectar pulsos en -4 dBm
+        send_command(instrument, ':SENSe:PULSe:RANGe 15E-6')  # Rango de tiempo de 15 μs
+        send_command(instrument, ':SENSe:PULSe:REFerence 0')  # Nivel de referencia en 0 dBm
+        print("Configurando la vista Pulse Trace...")
+        send_command(instrument, ':DISPlay:PULSe:MEASview:NEW TRACe')  # Selecciona la vista Pulse Trace
+        send_command(instrument, ':INITiate:IMMediate', wait_opc=False)  # Inicia la medición
+        opc_response = instrument.query('*OPC?').strip()  # Verifica que la operación haya terminado
+        print(f"Operación completada (Pulse Trace): {opc_response}")
+
+        instrument.write(':FETCh:PULSe:TRACe?')  # Solicita los datos de phase vs time
+        raw_response = instrument.read_raw()  # Lee los datos en formato binario
+        print(f"Datos crudos de Pulse Trace {raw_response[:50]}...")
+
+        # Procesa los datos binarios recibidos
+        if raw_response[0] == ord('#'):  # Verifica que el formato sea correcto (inicia con #)
+            num_digits = int(chr(raw_response[1]))  # Número de dígitos que indican la longitud de datos
+            num_bytes = int(raw_response[2:2 + num_digits].decode())  # Longitud de los datos en bytes
+            header_length = 2 + num_digits  # Longitud del encabezado
+            data_bytes = raw_response[header_length:header_length + num_bytes]  # Extrae los datos
+            pulse_data = struct.unpack(f'<{num_bytes // 4}f', data_bytes)  # Convierte bytes a flotantes
+            pulse_data = np.array(pulse_data)  # Convierte a arreglo NumPy
+            # Genera un arreglo de puntos temporales (de 0 a 15 μs)
+                
+            time = np.linspace(0, 15e-6, len(pulse_data)) * 1e6  # Convierte a microsegundos
+            # Filtrado suave para eliminar ruido extremo (valores fuera de -100 dBm a 20 dBm)
+            valid_mask = (pulse_data >= -100) & (pulse_data <= 20)
+
+            if np.any(valid_mask):
+                pulse_data = pulse_data[valid_mask]  # Aplica el filtro
+                time = time[valid_mask]  # Ajusta los tiempos correspondientes
+                # Guarda los datos en un archivo CSV
+                
+                # Busca el próximo número disponible para el archivo
+                i = 1
+                while os.path.exists(os.path.join(directorio, f'TimeOverview_{i}.csv')):  # Verifica si el archivo ya existe
+                    i += 1  # Incrementa el número
+
+                filename = f'PulseTrace_{i}.csv'
+                output_path = os.path.join(directorio, filename)
+
+                # Guarda los datos en un archivo CSV
+                pd.DataFrame({'Time (ms)': time, 'Amplitud (dBm)': pulse_data}).to_csv(output_path, index=False)
+                print(f"Datos guardados en '{output_path}'.")
+
+                if plot:
+                    ploter(output_path)
+            else:
+                print("No se encontraron datos válidos en Pulse Trace después de filtrar.")
+                pulse_data = np.array([])
+                time = np.array([])
+
+            else:
+                print("Formato de respuesta inesperado en Pulse Trace.")
+        return f"Medicion Exitosa"
+
+    except Exception as e:
+        return f"Error en Pulse Trace: {e}"  # Solicitar datos
 
