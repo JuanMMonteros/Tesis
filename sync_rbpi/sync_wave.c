cef32b9f43ab9917ce2e38447aec291cd41eb4f2
juan.m.int <juan.m.int@indiesemi.com>
2025-09-19 15:49:10 -0300

    change GPIO

--- sync_rbpi/sync_wave.c
+++ sync_rbpi/sync_wave.c
@@ -9,7 +9,7 @@
 #include <gpiod.h>
 
 #define GPIO_CHIP "/dev/gpiochip0"  // Chip principal
-#define GPIO_LINE 17                // Pin GPIO17 (BCM)
+#define GPIO_LINE 21                // Pin GPIO17 (BCM)
 #define PERIOD_NS 2000000L          // 2 ms en nanosegundos
 
 static int running = 1;
