3d64cd24f42d90aae1aa7d4f4691b5465397b272
joaquin <joaquindosmil@gmail.com>
2025-10-20 16:10:20 -0300

    v13

--- transmision-bladerf/src/main.c
+++ transmision-bladerf/src/main.c
@@ -182,9 +182,10 @@ int main(void)
         status = bladerf_sync_tx(dev, waveform, WAVEFORM_LEN / 2, NULL, 0);
 
         //One-shot: esperar a que el pulso del trigger cambie su estado
-        do {
-            bladerf_trigger_state(dev, &trigger,&is_armed, &fired,&fired_req,NULL, NULL);
-        } while (fired);
+        // do {
+        //     bladerf_trigger_state(dev, &trigger,&is_armed, &fired,&fired_req,NULL, NULL);
+        // } while (fired);
+        usleep(3700);  // Ajustar según el ancho del pulso de trigger
 
         //Re-armar trigger para la próxima iteración
         // status = bladerf_trigger_arm(dev, &trigger, false, 0, 0);
